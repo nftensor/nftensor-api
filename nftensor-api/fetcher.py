7e58b75a57c359a484137965ab4a8f5ba5a5bbcd
0xcacti <0xcacti@gmail.com>
2023-06-07 01:31:56 -0400

    add event handling, more work to manage this correctly

--- nftensor-api/fetcher.py
+++ nftensor-api/fetcher.py
@@ -2,14 +2,31 @@ from web3 import Web3
 import abi
 import generator 
 from loguru import logger
+import time
 
 # fetches queries from the NFTensor contract 
 def fetch_queries(endpoint, nftensor_address):
     # connect to the blockchain
-    w3 = Web3(Web3.HTTPProvider(endpoint))
+    # w3 = Web3(Web3.HTTPProvider(endpoint))
+    w3 = Web3(Web3.WebsocketProvider("ws://localhost:8545"))
     # get the contract
     contract = w3.eth.contract(address=nftensor_address, abi=abi.nftensor_abi)
+    event_filter = contract.events.Transfer.createFilter(fromBlock='latest')
+    block_filter = w3.eth.filter({'fromBlock':'latest', 'address':nftensor_address})
+    
 
+    start_time = time.time()
+    while True:
+        events = block_filter.get_new_entries()
+        for event in events:
+            handle_event(w3, event)
+
+        if time.time() - start_time > 500:
+            check_for_mints()
+            start_time = time.time()s
+
+ 
+def check_for_mints(): 
     # get a singular query 
     for i in range(1, 501):
         query = contract.functions.queries(i).call()
@@ -17,6 +34,7 @@ def fetch_queries(endpoint, nftensor_address):
             generator.generate_image(query, i)
         else:
             logger.info("no new queries found")
+
         
-        
-        
+def handle_event(provider, event):
+    txn_hash = provider.eth.waitForTransactionReceipt(event['transactionHash'])
