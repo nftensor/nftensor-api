2232a1ab157f37df6d0108a01b433a7da4c41d57
0xcacti <0xcacti@gmail.com>
2023-06-14 21:18:40 -0400

    fixing event subscription and breaking it apart

--- nftensor-api/fetcher.py
+++ nftensor-api/fetcher.py
@@ -1,8 +1,10 @@
 from web3 import Web3
 import abi
+import files
 import generator
 from loguru import logger
 import time
+import json
 
 
 # fetches queries from the NFTensor contract
@@ -12,34 +14,44 @@ def fetch_queries(endpoint, nftensor_address):
     # get the contract
     contract = w3.eth.contract(address=nftensor_address, abi=abi.nftensor_abi)
     event_filter = contract.events.Transfer.createFilter(fromBlock="latest")
-    block_filter = w3.eth.filter({"fromBlock": "latest", "address": nftensor_address})
 
     start_time = time.time()
     while True:
-        events = block_filter.get_new_entries()
+        events = event_filter.get_new_entries()
         for event in events:
-            handle_event(w3, event)
+            handle_event(w3, contract, event)
 
         if time.time() - start_time > 500:
-            check_for_mints()
+            check_for_mints(contract)
             start_time = time.time()
 
 
-def check_for_mints():
+def check_for_mints(contract):
     # get a singular query
     for i in range(1, 501):
-        query = contract.functions.queries(i).call()
-        if generator.image_exists(i):
+        handle_mint(contract, i)
+
+def handle_mint(contract, id):
+        query = contract.functions.queries(id).call()
+        if not files.image_exists(id):
             if query != "":
-                logger.info(f"new query found, re-generating image #{i}")
-                generator.generate_image(query, i)
+                logger.info(f"new query found, re-generating image #{id}")
+                generator.generate(query, id)
         else:
-            with open(f"/execute/assets/json/{i}", "r") as f:
-                data = json.load(f)
-                if query != data["attributes"][0]["value"]:
-                    logger.warn("re-org found, re-generating image")
-                    generator.generate_image(query, i)
+            # we have to fix this
+            if files.json_exists(id):
+                with open(f"/execute/assets/json/{id}", "r") as f:
+                    data = json.load(f)
+                    if query != data["attributes"][0]["value"]:
+                        logger.warn("re-org found, re-generating image")
+                        generator.generate(query, id)
+            else:
+                logger.warn("image exists, but json not generated, cleaning up for retry")
+                files.cleanup(id)
 
-
-def handle_event(provider, event):
+def handle_event(provider, contract, event):
     txn_hash = provider.eth.waitForTransactionReceipt(event["transactionHash"])
+    # check if the sender is 0 
+    print(txn_hash)
+    id = contract.functions.tokenID().call() - 1
+    handle_mint(contract, id)
